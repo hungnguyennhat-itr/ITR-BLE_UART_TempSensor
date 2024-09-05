28847ec6ff6c2c31a6442aab14dd18298129b750
Hung Nguyen <hungnguyennhat@itrvn.com>
2024-09-05 10:00:51 +0700

    feat: start BLE component on host and register event callback

--- BLE_UART_TempSensor.cydsn/main_cm4.c
+++ BLE_UART_TempSensor.cydsn/main_cm4.c
@@ -8,19 +8,34 @@
  * WHICH IS THE PROPERTY OF your company.
  *
  * ========================================
-*/
+ */
 #include "project.h"
+
+void genericstack_callback(uint32_t event, void *event_param);
+void ble_apphost_callback(void);
 
 int main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
-    for(;;)
+    if (Cy_BLE_Start(genericstack_callback) == CY_BLE_SUCCESS)
+    {
+        Cy_BLE_RegisterAppHostCallback(ble_apphost_callback);
+    }
+    else
+    {
+        CY_ASSERT(0u);
+    }
+    
+    for (;;)
     {
         /* Place your application code here. */
     }
 }
 
+void ble_apphost_callback(void)
+{
+    Cy_BLE_ProcessEvents(void);
+}
 /* [] END OF FILE */
