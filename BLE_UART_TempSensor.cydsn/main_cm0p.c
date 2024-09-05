bf333617e60b460af8216a6641a0e722b47db958
Hung Nguyen <hungnguyennhat@itrvn.com>
2024-09-05 09:58:53 +0700

    feat: start BLE component and proccess controller event

--- BLE_UART_TempSensor.cydsn/main_cm0p.c
+++ BLE_UART_TempSensor.cydsn/main_cm0p.c
@@ -8,20 +8,29 @@
  * WHICH IS THE PROPERTY OF your company.
  *
  * ========================================
-*/
+ */
 #include "project.h"
 
 int main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
     /* Enable CM4.  CY_CORTEX_M4_APPL_ADDR must be updated if CM4 memory layout is changed. */
-    Cy_SysEnableCM4(CY_CORTEX_M4_APPL_ADDR); 
-
+    if (Cy_BLE_Start(NULL) == CY_BLE_SUCCESS)
+    {
+        Cy_SysEnableCM4(CY_CORTEX_M4_APPL_ADDR);
+    }
+    else
+    {
+        /* Halt the CPU */
+        CY_ASSERT(0u);
+    }
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
-    for(;;)
+    for (;;)
     {
         /* Place your application code here. */
+        Cy_BLE_ProcessEvents();
+        Cy_SysPm_DeepSleep(CY_SYSPM_WAIT_FOR_INTERRUPT);
     }
 }
 
