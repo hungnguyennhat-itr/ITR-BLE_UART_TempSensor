9de53f7660efd4649e2ed8e9112a1fe1cba46a0e
Hung Nguyen <hungnguyennhat@itrvn.com>
2024-09-06 13:50:57 +0700

    test: testing receiving function over BLE

--- BLE_UART_TempSensor.cydsn/main_cm0p.c
+++ BLE_UART_TempSensor.cydsn/main_cm0p.c
@@ -11,24 +11,34 @@
  */
 #include "sys/sys_ble.h"
 
-uint8_t data[20] = {0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24,
-                    0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24};
-bool result = 0;
-int main(void)
+uint8_t data[20]       = { 0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24,
+                           0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24, 0x24 };
+bool    init_result    = 0;
+bool    receive_result = 0;
+uint8_t rcv_data;
+int     main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
     /* Enable CM4.  CY_CORTEX_M4_APPL_ADDR must be updated if CM4 memory layout is changed. */
     Cy_SysEnableCM4(CY_CORTEX_M4_APPL_ADDR);
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    result = sys_ble_init();
+    init_result = sys_ble_init();
     for (;;)
     {
         /* Place your application code here. */
-        if (result)
+        if (init_result)
         {
-            sys_ble_transmit(data, 20);
+            receive_result = sys_ble_receive(&rcv_data);
+            if (receive_result)
+            {
+                Cy_GPIO_Write(LED_GREEN_0_PORT, LED_GREEN_0_NUM, 0);
+                CyDelay(1000);
+            }
+            else
+            {
+                Cy_GPIO_Write(LED_GREEN_0_PORT, LED_GREEN_0_NUM, 1);
+            }
         }
-        
     }
 }
 
