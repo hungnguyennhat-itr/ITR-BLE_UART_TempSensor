b3d5a62f6e96c77f57dd1b89d2ff1e50db116fb2
Hung Nguyen <hungnguyennhat@itrvn.com>
2024-09-06 11:06:17 +0700

    refactor: clean unnecessary code in M0

--- BLE_UART_TempSensor.cydsn/main_cm0p.c
+++ BLE_UART_TempSensor.cydsn/main_cm0p.c
@@ -15,22 +15,14 @@ int main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
     /* Enable CM4.  CY_CORTEX_M4_APPL_ADDR must be updated if CM4 memory layout is changed. */
-    if (Cy_BLE_Start(NULL) == CY_BLE_SUCCESS)
-    {
-        Cy_SysEnableCM4(CY_CORTEX_M4_APPL_ADDR);
-    }
-    else
-    {
-        /* Halt the CPU */
-        CY_ASSERT(0u);
-    }
+    Cy_SysEnableCM4(CY_CORTEX_M4_APPL_ADDR);
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
     for (;;)
     {
         /* Place your application code here. */
-        Cy_BLE_ProcessEvents();
-        // Cy_SysPm_DeepSleep(CY_SYSPM_WAIT_FOR_INTERRUPT);
+        
+        
     }
 }
 
